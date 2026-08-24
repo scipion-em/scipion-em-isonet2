9fbdb27bd1f4d8918437218eb0d0b88e41630885
JorMaister <jorgejjs86@gmail.com>
2026-08-24 17:58:29 +0200

    Add `isonet2` plugin with citation and metadata initialization for Scipion.

---
+++ isonet2/bibtex.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+@article{Liu2022,
+title = "Isotropic reconstruction for electron tomography with deep learning",
+author = "Liu, Yun-Tao et al.",
+journal = "Nature Communications",
+year = "2022",
+volume = "13",
+number = "1",
+doi = "10.1038/s41467-022-33957-8",
+url = "http://doi.org/10.1038/s41467-022-33957-8",
+}
+
+"""
