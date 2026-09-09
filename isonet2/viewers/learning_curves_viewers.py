968677d8eab4a759f00534c086e9c58461f9dfcc
fra-cava <fracavallo163@gmail.com>
2026-09-09 10:52:51 +0200

    Standardize learning curves viewer to display `loss_full.png` with matplotlib integration.

---
+++ isonet2/viewers/learning_curves_viewers.py
@@ -0,0 +1,27 @@
+from matplotlib import pyplot as plt
+import pyworkflow.viewer as pwviewer
+from isonet2.protocols import ProtIsonet2Training
+from pwem.viewers import ImageView
+
+
+
+class DeepFinderLCurvesViewer(pwviewer.Viewer):
+    _label = 'Learning Curves Viewer'
+    _targets = [ProtIsonet2Training]
+
+    def _visualize(self, obj, **kwargs):
+        view = Isonet2ImageView(self.protocol._getModelOutDir('loss_full.png'))
+        view._tkParent = self.getTkRoot()
+        return [view]
+
+class Isonet2ImageView(ImageView):
+
+    def show(self):
+        image_file = self.getImagePath()
+        plt.figure(num='Isonet2 Learning Curves')
+        image = plt.imread(image_file)
+        plt.imshow(image)
+        plt.axis('off')
+        plt.tight_layout()  # Decrease the padding
+        plt.show()
+
